df212d65d309c6bedc3b67b6aa5f1a32237ef6af
JulianMorales2003 <pipe.moralesz07@gmail.com>
2026-05-11 12:07:34 -0500

    l

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 import presentation.MainMenuView;
 import javax.swing.*;
 
+/**
+ * Clase con el objetivo de iniciar el juego.
+ */
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
