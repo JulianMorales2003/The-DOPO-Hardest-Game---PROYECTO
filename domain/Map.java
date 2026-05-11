8299cee13d700fda5d97fc138b90e9ecb4f03f33
JulianMorales2003 <pipe.moralesz07@gmail.com>
2026-05-10 22:18:04 -0500

    Se cambia de protectes a public los atributos de la clase map.

--- domain/Map.java
+++ domain/Map.java
@@ -2,22 +2,24 @@ package domain;
 
 import java.util.List;
 import java.util.ArrayList;
+
 /**
  * Representa el tablero de un nivel del juego.
  */
 public class Map {
 
-    protected int width;
-    protected int height;
-    protected int cellSize;
-    protected List<Coin> coins;
-    protected List<Enemy> enemies;
-/**
-* Crea un mapa con las dimensiones indicadas.
-*
-* @param width  ancho del tablero en píxeles
-* @param height alto del tablero en píxeles
-*/
+    public int width;
+    public int height;
+    public int cellSize;
+    public List<Coin> coins;
+    public List<Enemy> enemies;
+
+    /**
+     * Crea un mapa con las dimensiones indicadas.
+     *
+     * @param width  ancho del tablero en píxeles
+     * @param height alto del tablero en píxeles
+     */
     public Map(int width, int height) {
         this.width    = width;
         this.height   = height;
